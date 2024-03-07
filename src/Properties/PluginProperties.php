263ac54486504a00d2488c9b09d70541acdde186
Moritz Meißelbach <m.meisselbach@inpsyde.com>
2024-03-07 11:23:55 +0100

    Avoid early wptexturize
    Signed-off-by: Moritz Meißelbach <m.meisselbach@inpsyde.com>

--- src/Properties/PluginProperties.php
+++ src/Properties/PluginProperties.php
@@ -84,8 +84,9 @@ class PluginProperties extends BaseProperties
         if (!function_exists('get_plugin_data')) {
             require_once ABSPATH . 'wp-admin/includes/plugin.php';
         }
-
-        $pluginData = get_plugin_data($pluginMainFile);
+        
+        // $markup = false, to avoid an incorrect early wptexturize call. Also we probably don't want HTML here anyway
+        $pluginData = get_plugin_data($pluginMainFile, false);
         $properties = Properties::DEFAULT_PROPERTIES;
 
         // Map pluginData to internal structure.
