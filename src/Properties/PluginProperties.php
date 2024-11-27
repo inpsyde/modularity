03af2d4ac22e816f6ee4bf6769ef2afe7e2793cd
Moritz Meißelbach <m.meisselbach@inpsyde.com>
2024-11-27 15:54:44 +0100

    Revert "Only skip translations if loaded early."
    This reverts commit 2108ee299413385a5459c2cc5394f011d54571de.

--- src/Properties/PluginProperties.php
+++ src/Properties/PluginProperties.php
@@ -54,12 +54,11 @@ class PluginProperties extends BaseProperties
             require_once ABSPATH . 'wp-admin/includes/plugin.php';
         }
 
-        // Avoid implicitly loading translations too early
-        // @see https://core.trac.wordpress.org/changeset/59127
-        $translate = did_action('after_setup_theme') || doing_action('after_setup_theme');
         // $markup = false, to avoid an incorrect early wptexturize call.
+        // $translate = false, to avoid loading translations too early
         // @see https://core.trac.wordpress.org/ticket/49965
-        $pluginData = (array) get_plugin_data($pluginMainFile, false, $translate);
+        // @see https://core.trac.wordpress.org/ticket/34114
+        $pluginData = (array) get_plugin_data($pluginMainFile, false, false);
         $properties = Properties::DEFAULT_PROPERTIES;
 
         // Map pluginData to internal structure.
